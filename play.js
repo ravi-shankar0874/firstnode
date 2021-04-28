a9723ff284f130e316e0437e7d1087f607f91b94
ravi-shankar0874 <83342302+ravi-shankar0874@users.noreply.github.com>
2021-04-28 15:22:24 +0530

    Add files via upload

---
+++ play.js
@@ -0,0 +1,10 @@
+var name='max';
+var age=20;
+var hasHobbies=true;
+
+function sumrizeUser(userName,userAge,userHasHobbies)
+{
+    return 'Name is'+userName+'age is'+userAge+'hobbies is'+userHasHobbies;
+}
+
+console.log(sumrizeUser(name,age,hasHobbies));
